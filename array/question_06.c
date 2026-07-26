a833c667fd42fbeb266dbba7af741960dd44b8f9
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-26 19:58:05 +0530

    missing element in the array

---
+++ array/question_06.c
@@ -0,0 +1,16 @@
+// FIND MISSING ELEMENT IN THE ARRAY
+
+#include <stdio.h>
+
+int main(){
+    int arr[] = {1,2,3,4,5,7};
+    int n = sizeof(arr)/sizeof(arr[0]) +1;
+    int sum = n*(n+1)/2;
+    int arrsum = 0;
+
+    for (int i = 0; i<n-1;i++){
+        arrsum = arrsum + arr[i];
+    }
+    printf("%d" , sum-arrsum);
+    return 0;
+}
