75fcf037c29892b408613c8812ba54d6039925bd
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-26 20:01:38 +0530

    reverse the array

---
+++ array/question_11.c
@@ -0,0 +1,23 @@
+// REVERSE THE ARRAY
+
+#include <stdio.h>
+
+int main(){
+    int arr[] = {2,3,1,5,6,9,8,11,23};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int i = 0;
+    int j = n-1;
+
+    while(i<j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    for(int i = 0; i<n ;i++){
+        printf("%d " , arr[i]);
+    }
+    return 0;
+}
