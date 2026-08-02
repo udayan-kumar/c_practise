985370958e755975c21afe4e85e6068cb1914133
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-02 23:40:48 +0530

    question 02

---
+++ sorting/question_02.c
@@ -0,0 +1,24 @@
+//! BUBBLE SORT IN ASCINDING ORDER
+
+#include <stdio.h>
+
+int main(){
+    int arr[] = {3,4,1,2,5,3,6,7,5,8};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i = 0; i<n-1; i++){
+        for(int j = 0; j<n-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i<n; i++){
+        printf("%d " , arr[i]);
+    }
+    return 0;
+}
