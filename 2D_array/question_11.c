0a760e739786ac49c92522ef0f7a8e7dd20c992f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-02 16:16:12 +0530

    find sum of all the element of the 2d array

---
+++ 2D_array/question_11.c
@@ -0,0 +1,17 @@
+//!  FIND SUM OF ALL THE ELEMENT IN THE 2D ARRAY
+
+#include <stdio.h>
+
+int main(){
+    int arr[4][4] = {{1,2,3,4}, {5,6,7,8},{9,10,11,12}, {13,14,15,16}};
+
+    int sum = 0;
+
+    for(int i = 0; i<4; i++){
+        for(int j = 0; j<4; j++){
+            sum += arr[i][j];
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
